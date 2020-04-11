667307638fc5a29ed3127f29077f69d0c6eb13a5
Anderson Silva <anderson.silva@indigosoft.com.br>
2020-04-11 18:39:47 -0300

    ajusto de espaços vazios

---
+++ login/login_efetuar.py
@@ -0,0 +1,20 @@
+json = {"email":"a.pissulin@gmail.com", "senha":"123456", "perfil":"adm"} # Retorno do banco de dados 
+
+class Login_efetuar:
+    """ Classe para efetuar o login no sistema """
+   
+    def __init__(self, email, senha):
+        """ Email e senha vem do formulario do front """
+        self.email = email
+        self.senha = senha 
+         
+         
+
+    def login_check(self):
+        """ Metodo para checar se o login é valido  """
+        global json 
+        if(self.email == json["email"] and self.senha == json["senha"]):
+            print("Esta logado no minha conta: [ perfil:",json["perfil"],"]") # aqui pode ser retorna um True 
+        else:
+            print("Email ou senha invalida!") # aqui pode ser retorna um False 
+        
---
+++ main.py
@@ -0,0 +1,7 @@
+from login.login_efetuar import Login_efetuar
+
+email =input("digite seu email: ")
+senha = input("digite sua senha : ")
+
+Login_efetuar(email, senha).login_check()
+
